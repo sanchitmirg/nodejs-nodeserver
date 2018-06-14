88f1f65c706e7f771798e4fdc5bb3f5e92c20ec9
Sanchit Mirg <sanchitmirg0@gmail.com>
2018-06-14 21:58:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+//hbs is handlebars which is helpful in creating template driven page
+const hbs = require('hbs');
+//registering partial means when we take out the common code from html file like footer and title
+//and keep it in different files for that we have to register partials
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine', 'hbs');
+
+//we are tweaking express when request comes in to first do this task
+//setting to public directory to render static pages.
+app.use(express.static(__dirname+'/public'));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World Again!!!");
+})
+
+app.get('/about', (req, res) => {
+    //the second argument will go into hbs and we can use title key like this {{title}} to
+    //replace it with lable.
+    res.render('about.hbs', {
+        title:'This is about page through Templating'
+    })
+})
+app.listen(3000);
